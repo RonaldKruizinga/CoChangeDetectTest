95752c69f8d84ba3187fdc2616a965fb74c7f29f
Ronald Kruizinga <ronmatk@gmail.com>
2020-03-12 12:09:22 +0100

    Cochange

--- TestingRepository/src/Cyclic.java
+++ TestingRepository/src/Cyclic.java
@@ -2,4 +2,8 @@ public class Cyclic {
     public static void cyclicMethod(){
         Main.anotherStaticMethod();
     }
+
+    public static String MethodFactoryBuilderLogger(){
+        return "Java has long method names in enterprise";
+    }
 }
--- TestingRepository/src/Independent.java
+++ TestingRepository/src/Independent.java
@@ -1,2 +1,14 @@
 public class Independent {
+    public static class SubClass{
+        int x;
+    }
+
+    public static SubClass instance;
+
+    public static SubClass getInstance(){
+        if(instance == null){
+            instance = new SubClass();
+        }
+        return instance;
+    }
 }
