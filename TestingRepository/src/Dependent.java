d5b146515771b9ab47cfbf6e29292fbf5efda94a
Ronald Kruizinga <ronmatk@gmail.com>
2020-03-12 12:46:50 +0100

    commit on branch 1

--- TestingRepository/src/Dependent.java
+++ TestingRepository/src/Dependent.java
@@ -1,5 +1,10 @@
 public class Dependent {
     public Dependent(){
         Cyclic.cyclicMethod();
+        Cyclic.MethodFactoryBuilderLogger();
+    }
+
+    public String rant(){
+        return "I still feel like string should be an acceptable primitive like in C#";
     }
 }
