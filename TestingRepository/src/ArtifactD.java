921312e16b57fb9befc2203e7e51a15692496d75
Ronald Kruizinga <ronmatk@gmail.com>
2020-03-12 12:18:18 +0100

    Change A

--- TestingRepository/src/ArtifactD.java
+++ TestingRepository/src/ArtifactD.java
@@ -1,5 +1,17 @@
+import java.util.ArrayList;
+
 public class ArtifactD {
     public static int method(int x){
         return 2*x;
+    }
+
+    private ArrayList<ArtifactD> children;
+
+    public ArtifactD(){
+        children = new ArrayList<>();
+    }
+
+    public void addChild(ArtifactD d){
+        children.add(d);
     }
 }
