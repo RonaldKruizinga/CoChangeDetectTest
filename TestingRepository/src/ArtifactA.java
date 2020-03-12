a9fc30e55992676e953e11ad8c35070050af7331
Ronald Kruizinga <ronmatk@gmail.com>
2020-03-12 12:19:06 +0100

    Change B

--- TestingRepository/src/ArtifactA.java
+++ TestingRepository/src/ArtifactA.java
@@ -2,4 +2,9 @@ public class ArtifactA {
     public static ArtifactA Create(){
         return new ArtifactA();
     }
+
+    public ArtifactA(){
+        ArtifactD d = new ArtifactD();
+        d.addChild(d);
+    }
 }
